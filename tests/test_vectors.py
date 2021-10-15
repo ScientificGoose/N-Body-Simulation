d6b1627532637f2e5f528e984ca5cd8c0f716334
Rick Burta <rick@circleci.com>
2021-10-14 21:07:38 -0400

    Split Test.py into separate files under tests/ :truck: :alembic:

---
+++ tests/test_vectors.py
@@ -0,0 +1,19 @@
+import pytest
+from src.models import Vector2D, Vector3D
+
+
+def vector_test():
+    # vec1 = Vector2D(1.0, 5.5)
+    # vec2 = Vector2D(6.6, 12.0)
+    # #print(f'x = {vec1.x}\ny = {vec1.y}')
+    # vec1.add(vec2)
+    # #print(f'\nx = {vec1.x}\ny = {vec1.y}')
+    # #print(f"\nDistance between: {vec1.get_distance_between(vec2)}")
+    # vec3 = Vector3D(1.0, 5.5, 5.6)
+    # vec4 = Vector3D(6.6, 12.0, 6.8)
+    # vec5 = vec3.get_difference_vector(vec4)
+    # print(f'x = {vec5.x}\ny = {vec5.y}\nz = {vec5.z}')
+    # vec3.add(vec4)
+    # #print(f'\nx = {vec3.x}\ny = {vec3.y}\nz = {vec3.z}')
+    # #print(f"\nDistance between: {vec3.get_distance_between(vec4)}")
+    pass
